7e9c126079c806a3f7217c34b5db29d471bd8ea5
boudewijn overvliet <boudewijn@hello-data.nl>
2023-05-24 11:03:36 +0200

    remove unused code

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 import dotenv from 'dotenv'
 import nodefetch from 'node-fetch'
 
-// get params from command line and put in associative array
-const args = process.argv.slice(2).reduce((acc, arg) => {
-    const [key, value] = arg.split('=')
-    acc[key] = value
-    return acc
-}, {})
-
 dotenv.config();
 
 const getFolders = async () => {
     const url = 'http://localhost:41184/folders?token='
     + process.env.JOPLIN_TOKEN
-    const response = await nodefetch(url, {
-        method: 'GET',
-    })
-
-    return response.json()
-}
-
-const getNotes = async (page) => {
-    const url = 'http://localhost:41184/notes?has_more=true&token='
-    + process.env.JOPLIN_TOKEN
-    + '&page='
-    + page
-    + '&fields=id,title,body,parent_id,created_time,order'
     const response = await nodefetch(url, {
         method: 'GET',
     })
@@ -94,10 +74,3 @@ getFolders().then(data => {
         })
     })
 })
-
-/*getNotes(args['--page'] ? args['--page'] : 0).then(data => data.items.map(item => {
-    const tags = getNoteTags(item.id).then(data => {
-        const tagNames = data.items.map(item => item.title)
-        if(tagNames.length > 0) console.log(item.title, tagNames)
-    })
-}))*/
